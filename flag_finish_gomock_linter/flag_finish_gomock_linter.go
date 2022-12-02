a4ca5bfeeebd05908c2321bd68969adf80dad41d
daikidev111 <dkub0001@student.monash.edu>
2022-12-02 11:24:54 +0800

    inline comment added

--- flag_finish_gomock_linter/flag_finish_gomock_linter.go
+++ flag_finish_gomock_linter/flag_finish_gomock_linter.go
@@ -2,7 +2,6 @@ package flag_finish_gomock_linter
 
 import (
 	"go/ast"
-	"log"
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
@@ -23,24 +22,17 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (any, error) {
-    // fset := token.NewFileSet()
-    // f, err := parser.ParseFile(fset, "./testdata/src/a/a.go", nil, 0)
-	// if err != nil {
-	// 	panic(err)
-	// }
-    // ast.Print(fset, f)
-
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
 		(*ast.CallExpr)(nil),
 	}
-	go_mock_use_flag := false
+
+	go_mock_use_flag := false // once true, it stays true for the rest of preorder traversal process
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch callExprTyp := n.(type) {
 		case *ast.CallExpr:
-			// ast.Print(fset, n)
 			go_mock_use_finish := false
 
 			// new_cont_flag := false
@@ -55,16 +47,15 @@ func run(pass *analysis.Pass) (any, error) {
 				return
 			}
 			target_node, ok := callExprTyp.Fun.(*ast.SelectorExpr)
-			if !ok {
+			if !ok { // if target node is not detected, break to prevent from throwing panic
 				break
 			}
 			if (target_node.Sel.Name == "Finish") {
 				go_mock_use_finish = true
 			}
 
-			if (go_mock_use_finish && go_mock_use_flag) {
-				log.Print(target_node.Sel.NamePos, target_node.Sel.Name)
-				pass.Reportf(target_node.Sel.NamePos, "identifier is GoMock Finish")
+			if (go_mock_use_finish && go_mock_use_flag) { // if both true, finish and gomock used
+				pass.Reportf(target_node.Sel.NamePos, "identifier is GoMock Finish") 
 			}
 
 		} 
