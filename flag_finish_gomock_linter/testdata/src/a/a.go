86dd5769e12cfca86a6d8e9d0fc925d8ded61620
daikidev111 <dkub0001@student.monash.edu>
2022-12-02 11:25:17 +0800

    added more testcases

--- flag_finish_gomock_linter/testdata/src/a/a.go
+++ flag_finish_gomock_linter/testdata/src/a/a.go
@@ -11,11 +11,11 @@ func okFirst(t *testing.T) {
 	mock.Finish() // want "identifier is GoMock Finish"
 }
 
-// func okSecond(t *testing.T) {
-// 	mock := gomock.NewController(t)
-// 	defer mock.Finish() // want "identifier is GoMock Finish"
-// }
+func okSecond(t *testing.T) {
+	mock := gomock.NewController(t)
+	defer mock.Finish() // want "identifier is GoMock Finish"
+}
 
-// func  failFirst(t *testing.T) {
-// 	gomock.NewController(t)
-// }
+func  failFirst(t *testing.T) {
+	gomock.NewController(t)
+}
